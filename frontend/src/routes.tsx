e285ce9baec53c43d6766d984632e9f178540afe
dltjddn0051 <dltjddn0051@gmail.com>
2025-11-28 14:46:20 +0900

    강의 등록 및 강의 계획서 다시 만듦

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -249,7 +249,7 @@ const AppRoutes = ({ user, onLogout }: { user: User; onLogout: () => void }) => 
                 path="/professor/syllabus"
                 element={
                   <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-                    <ProfessorSyllabus />
+                    <ProfessorSyllabus user={user} />
                   </div>
                 }
               />
